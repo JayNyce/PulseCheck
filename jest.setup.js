dd148404341a06b2f7e75b5578bd315da8f3da78
JayNyce <38516656+JayNyce@users.noreply.github.com>
2024-12-03 13:14:57 -0600

    added changes

--- jest.setup.js
+++ jest.setup.js
@@ -6,9 +6,4 @@ require('@testing-library/jest-dom');
 import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
-<<<<<<< HEAD
 global.TextDecoder = TextDecoder;
-=======
-global.TextDecoder = TextDecoder;
-
->>>>>>> 77f930577d6f878e48ed4d2165670eacb47ec513
